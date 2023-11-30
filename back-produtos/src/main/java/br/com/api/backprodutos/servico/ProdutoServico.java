cb4a455cfc6675cabdd2fb5bf091815ab071b28a
rjunio98 <robledorjunior1@gmail.com>
2023-11-30 17:40:46 -0300

    serviço para listar produtos

--- back-produtos/src/main/java/br/com/api/backprodutos/servico/ProdutoServico.java
+++ back-produtos/src/main/java/br/com/api/backprodutos/servico/ProdutoServico.java
@@ -1,5 +1,18 @@
 package br.com.api.backprodutos.servico;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.api.backprodutos.modelo.ProdutoModelo;
+import br.com.api.backprodutos.repositorio.ProdutoRepositorio;
+
+@Service
 public class ProdutoServico {
     
+    @Autowired
+    private ProdutoRepositorio pr;
+
+    public Iterable<ProdutoModelo> listar(){
+        return pr.findAll();
+    }
 }
