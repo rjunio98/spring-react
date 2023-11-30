e3ea5eb2bfc98b11c438d3b5b02afc23b21055e9
rjunio98 <robledorjunior1@gmail.com>
2023-11-30 17:05:13 -0300

    configurando controller

--- back-produtos/src/main/java/br/com/api/backprodutos/controle/ProdutoControle.java
+++ back-produtos/src/main/java/br/com/api/backprodutos/controle/ProdutoControle.java
@@ -1,5 +1,13 @@
 package br.com.api.backprodutos.controle;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProdutoControle {
-    
+
+    @GetMapping("/")
+    public String rota(){
+        return "Rota de produtos";
+    }
 }
