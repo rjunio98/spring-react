60545ab55a493929ab3debd999185c9b64d00b0f
rjunio98 <robledorjunior1@gmail.com>
2023-11-30 18:08:10 -0300

    serviço para cadastrar produto

--- back-produtos/src/main/java/br/com/api/backprodutos/servico/ProdutoServico.java
+++ back-produtos/src/main/java/br/com/api/backprodutos/servico/ProdutoServico.java
@@ -1,9 +1,13 @@
 package br.com.api.backprodutos.servico;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import br.com.api.backprodutos.modelo.ProdutoModelo;
+import br.com.api.backprodutos.modelo.RespostaModelo;
 import br.com.api.backprodutos.repositorio.ProdutoRepositorio;
 
 @Service
@@ -12,7 +16,22 @@ public class ProdutoServico {
     @Autowired
     private ProdutoRepositorio pr;
 
+    @Autowired
+    private RespostaModelo rm;
+
     public Iterable<ProdutoModelo> listar(){
         return pr.findAll();
+    }
+
+    public ResponseEntity<?> cadastrar(ProdutoModelo pm){
+        if(pm.getNome().equals("")){
+            rm.setMensagem("O nome do produto não pode ser vazio!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else if(pm.getMarca().equals("")){
+            rm.setMensagem("A marca do produto não pode ser vazia!");
+            return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
+        }else{
+            return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+        }
     }
 }
