501a2bc50733d1660ff77e0e3a70a6b77fed8a25
rjunio98 <robledorjunior1@gmail.com>
2023-11-30 22:11:27 -0300

    rota para alterar produtos

--- back-produtos/src/main/java/br/com/api/backprodutos/controle/ProdutoControle.java
+++ back-produtos/src/main/java/br/com/api/backprodutos/controle/ProdutoControle.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,6 +15,12 @@ import br.com.api.backprodutos.servico.ProdutoServico;
 public class ProdutoControle {
     @Autowired
     private ProdutoServico ps;
+
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm,"alterar");
+    }
+    
 
     @PostMapping("/cadastrar")
     public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
