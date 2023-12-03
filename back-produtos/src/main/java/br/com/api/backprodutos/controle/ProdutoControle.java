223453abde7e9ffdcc04529a0ecb36a5425a88c4
rjunio98 <robledorjunior1@gmail.com>
2023-12-03 00:40:45 -0300

    controle do produto

--- back-produtos/src/main/java/br/com/api/backprodutos/controle/ProdutoControle.java
+++ back-produtos/src/main/java/br/com/api/backprodutos/controle/ProdutoControle.java
@@ -2,19 +2,29 @@ package br.com.api.backprodutos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.backprodutos.modelo.ProdutoModelo;
+import br.com.api.backprodutos.modelo.RespostaModelo;
 import br.com.api.backprodutos.servico.ProdutoServico;
 
 @RestController
+@CrossOrigin(origins = "*")
 public class ProdutoControle {
     @Autowired
     private ProdutoServico ps;
+
+    @DeleteMapping("/remover/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable long codigo){
+        return ps.remover(codigo);
+    }
 
     @PutMapping("/alterar")
     public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
